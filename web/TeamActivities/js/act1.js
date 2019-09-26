7eeb999393450bab39992163c3df900ce01b4294
HoweBekah <how18019@byui.edu>
2019-09-25 22:09:42 -0700

    team activity finished

--- web/TeamActivities/js/act1.js
+++ web/TeamActivities/js/act1.js
@@ -5,5 +5,6 @@ function changeColor() {
   var firstDiv = document.getElementById("div1");
   var newColor = document.getElementById("pickColor").value;
   newColor = newColor.replace(" ", "");
+
   firstDiv.style.backgroundColor = newColor;
 }
