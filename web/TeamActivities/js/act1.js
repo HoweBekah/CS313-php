e14b8c8d34dc9f4a009386e7b3d2ed61ee83b8be
HoweBekah <how18019@byui.edu>
2019-09-25 22:24:31 -0700

    weird

--- web/TeamActivities/js/act1.js
+++ web/TeamActivities/js/act1.js
@@ -5,6 +5,6 @@ function changeColor() {
   var firstDiv = document.getElementById("div1");
   var newColor = document.getElementById("pickColor").value;
   newColor = newColor.replace(" ", "");
-
+  newColor = "#" + newColor;
   firstDiv.style.backgroundColor = newColor;
 }
