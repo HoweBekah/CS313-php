9b2e9defc7827da6ad8fdb0ae7000b1ecf0a5e19
HoweBekah <how18019@byui.edu>
2019-09-27 23:44:38 -0700

    Initial homepage creation with css

--- web/index.php
+++ web/index.php
@@ -1,5 +1,37 @@
 <?php
 
-  phpinfo();
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/main.css" />
+    <title>Bekah Howe CS 313 | Home </title>
+</head>
+<body>
+<header>
+        <img src="images/webMeandJett.jpg" alt="My profile picture">
+        <h1>Bekah Howe</h1>
+        <h2>CS 313 Homepage</h2>
+
+    </header>
+    <main>
+      
+        <ul id="assignmentnames">
+            <li>Assignment 1: <a href="#">??</a></li>
+            <li>Assignment 2: <a href="#">???</a></li>
+            <li>Assignment 3: <a href="#">????</a></li>
+        </ul>
+
+    </main>
+    <footer>
+        <p>
+            &copy;2019 | Bekah Howe
+        </p>
+    </footer>
+</body>
+</html>
+
