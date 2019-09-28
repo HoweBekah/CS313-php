9bc7f773f22575b60cf104eb5897fc4c1a3da0fb
HoweBekah <how18019@byui.edu>
2019-09-28 00:17:25 -0700

    underline smaller and rearranged title of doc

--- web/index.php
+++ web/index.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/main.css" />
-    <title>Bekah Howe CS 313 | Home </title>
+    <title>Home ~ Bekah Howe CS 313</title>
 </head>
 <body>
 <header>
