20f8d3fe8a6c17078dbd1de0a71d3575335aa1ee
HoweBekah <how18019@byui.edu>
2019-09-28 14:44:28 -0700

    nav links updated

--- web/nav.php
+++ web/nav.php
@@ -1,6 +1,6 @@
 <nav>
     <ul>
-        <li>Home</li>
-        <li>Assignments</li>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="assignments.php">Assignments</a></li>
     </ul>
 </nav>
