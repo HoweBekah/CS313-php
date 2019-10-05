e8861ac3dfcb6cd605d3e933861d01455d785ee9
HoweBekah <how18019@byui.edu>
2019-10-04 21:30:28 -0700

    added pictures to items

--- web/shoppingCart/itemBrowse.php
+++ web/shoppingCart/itemBrowse.php
@@ -15,55 +15,56 @@
    <div id="shirtDiv">
    <h2 id="shirtTitle">Shirts</h2>
    <section>
-   <h3></h3>
-   <img>
+   <h3>I'm an Angry Elf</h3>
+   <img src="images/angryElf.jpg" alt="I'm an angry elf shirt">
    <input type="button" value="Add to Cart" id="addCart">
    </section>
    <section>
-   <h3></h3>
-   <img>
+   <h3>Chubby Girls Cuddle Better</h3>
+   <img src="images/chubbyGirls.jpg" alt="Chubby girls cuddle better shirt">
    <input type="button" value="Add to Cart" id="addCart">
    </section>
    <section>
-   <h3></h3>
-   <img>
+   <h3>Cupcake Looking for Stud Muffin</h3>
+   <img src="images/cupcake.jpg" alt="Just a cupcake looking for a stud muffin shirt">
    <input type="button" value="Add to Cart" id="addCart">
    </section>
    <section>
-   <h3></h3>
-   <img>
+   <h3> Dexter's Lab</h3>
+   <img src="images/dexter.jpg" alt="Shirt with characters from Dexter's Laboratory">
    <input type="button" value="Add to Cart" id="addCart">
-   </section> <section>
-   <h3></h3>
-   <img>
+   </section> 
+   <section>
+   <h3>I Am Not Fast</h3>
+   <img src="images/notFast.jpg" alt="I am not fast Baymax shirt">
    <input type="button" value="Add to Cart" id="addCart">
    </section>
    <section>
-   <h3></h3>
-   <img>
+   <h3>Reptar is My Homeboy</h3>
+   <img src="images/reptar.jpg" alt="Reptar is my homeboy shirt">
    <input type="button" value="Add to Cart" id="addCart">
    </section>
 </div>
 <div id="onesieDiv">
    <h2  id="onesieTitle">Onesies</h2>
    <section>
-   <h3></h3>
-   <img>
+   <h3>Mommy's Golden Snitch</h3>
+   <img src="images/goldenSnitch.jpg" alt="onesie with golden snitch on it">
    <input type="button" value="Add to Cart" id="addCart">
    </section>
    <section>
-   <h3></h3>
-   <img>
-   <input type="button" value="Add to Cart" id="addCart">
-   </section>
-   <section id="newRow">
-   <h3></h3>
-   <img>
+   <h3>Hakuna Matata</h3>
+   <img src="images/hakuna.jpg" alt="onesie with simba on it that says Hakuna Matata">
    <input type="button" value="Add to Cart" id="addCart">
    </section>
    <section>
-   <h3></h3>
-   <img>
+   <h3>Stud Muffin</h3>
+   <img src="images/studMuffin.jpg" alt="onesie with stud muffin on it">
+   <input type="button" value="Add to Cart" id="addCart">
+   </section>
+   <section>
+   <h3>Too Cute I Am</h3>
+   <img src="images/tooCute.jpg" alt="onesie with yoda that says too cute I am">
    <input type="button" value="Add to Cart" id="addCart">
    </section>
 </div>
