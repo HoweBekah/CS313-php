1807d14f9681798c1a8c9ab27788b2414896066f
HoweBekah <how18019@byui.edu>
2019-10-04 19:24:52 -0700

    sections for items created

--- web/shoppingCart/itemBrowse.php
+++ web/shoppingCart/itemBrowse.php
@@ -8,7 +8,68 @@
 </head>
 <body>
    <header>
-   <img src="images/BerkshireTeeFarmsLogo.jpg">
-   </header> 
+   <img src="images/BerkshireTeeFarmsLogo.jpg" alt="Logo for Berkshire Tee Farms" id="logo">
+   <div id="title">
+   <h1>Berkshire Tee Farms</h1>
+   </div>
+   </header>
+   <main>
+   <div id="shirtDiv">
+   <h2>Shirts</h2>
+   <section>
+   <h3></h3>
+   <img>
+   <input type="button" value="Add to Cart" id="addCart">
+   </section>
+   <section>
+   <h3></h3>
+   <img>
+   <input type="button" value="Add to Cart" id="addCart">
+   </section>
+   <section>
+   <h3></h3>
+   <img>
+   <input type="button" value="Add to Cart" id="addCart">
+   </section>
+   <section>
+   <h3></h3>
+   <img>
+   <input type="button" value="Add to Cart" id="addCart">
+   </section> <section>
+   <h3></h3>
+   <img>
+   <input type="button" value="Add to Cart" id="addCart">
+   </section>
+   <section>
+   <h3></h3>
+   <img>
+   <input type="button" value="Add to Cart" id="addCart">
+   </section>
+</div>
+<div id="onesieDiv">
+   <h2>Onesies</h2>
+   <section>
+   <h3></h3>
+   <img>
+   <input type="button" value="Add to Cart" id="addCart">
+   </section>
+   <section>
+   <h3></h3>
+   <img>
+   <input type="button" value="Add to Cart" id="addCart">
+   </section>
+   <section>
+   <h3></h3>
+   <img>
+   <input type="button" value="Add to Cart" id="addCart">
+   </section>
+   <section>
+   <h3></h3>
+   <img>
+   <input type="button" value="Add to Cart" id="addCart">
+   </section>
+</div>
+   </main>
+   <footer></footer> 
 </body>
 </html>
