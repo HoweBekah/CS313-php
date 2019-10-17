cc460a87bcb0e0018ac5ad3d4b18735e87ca7b12
HoweBekah <how18019@byui.edu>
2019-10-16 19:55:27 -0700

    connected to DB?

---
+++ web/HoweCookbook/index.php
@@ -0,0 +1,41 @@
+<?php  
+try {
+		
+    // Get the various parts of the DB Connection from the URL
+    $dbopts = parse_url($dbUrl);
+    $dbHost = $dbopts["host"];
+    $dbPort = $dbopts["port"];
+    $dbUser = $dbopts["user"];
+    $dbPassword = $dbopts["pass"];
+    $dbName = ltrim($dbopts["path"],'/');
+    // Create the PDO connection
+    $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+    // this line makes PDO give us an exception when there are problems, and can be very helpful in debugging!
+    $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+}
+catch (PDOException $ex) {
+    // If this were in production, you would not want to echo
+    // the details of the exception.
+    echo 'Error!: ' . $ex->getMessage();
+    die();
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Howe Home</title>
+</head>
+<body>
+    <h1>Beverages</h1>
+    <?php
+    foreach ($db->query("SELECT * FROM recipes WHERE category = 'Beverage'") as $row) {
+     echo $row['recipe_name'] ;
+       }
+    ?>
+</body>
+</html>
