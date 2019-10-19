fefb116467c7d7f8ae8d9bfd7ad697f7501ce85c
HoweBekah <how18019@byui.edu>
2019-10-18 22:03:49 -0700

    dfgbnm

--- web/HoweCookbook/categoryRecipes.php
+++ web/HoweCookbook/categoryRecipes.php
@@ -35,7 +35,7 @@ $stmt->execute();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="CSS/main.css" />
-    <title><?php echo $categoryName; ?> Recipes</title>
+    <title><?php echo $categoryName['category']; ?> Recipes</title>
 </head>
 
 <body>
