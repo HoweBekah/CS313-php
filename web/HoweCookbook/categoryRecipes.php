f6e86daf2f493c9c1120b144d1e696dba2ccad38
HoweBekah <how18019@byui.edu>
2019-10-18 19:12:04 -0700

    fdghjyk

--- web/HoweCookbook/categoryRecipes.php
+++ web/HoweCookbook/categoryRecipes.php
@@ -23,6 +23,7 @@ catch (PDOException $ex) {
 
 session_start();
 //echo $_GET['id'];
+echo $db->query('SELECT category FROM category WHERE catid ='. $_GET['id']);
 $categoryName = $db->query('SELECT category FROM category WHERE catid ='. $_GET['id']);
 ?>
 <!DOCTYPE html>
