25873c10e7011b0b5e9653274399ac16cab91d36
HoweBekah <how18019@byui.edu>
2019-10-24 22:12:17 -0700

    maybe...

--- web/HoweCookbook/recipeDetails.php
+++ web/HoweCookbook/recipeDetails.php
@@ -28,7 +28,7 @@ $stmt->execute();
 <a href="index.php"><h1 id="howeHeader">Howe Family Cookbook</h1></a>
 </header>
 <main>
-    <form method="POST" action="updateRecipe.php" > 
+    <form method="POST" action="updateRecipe.php?recipeid=<?php $_GET['recipeid']?>" > 
    <h1 id="pageTitle"><?php echo $recipeInfo['recipe_name']; ?></h1>
    <h2>Ingredients:</h2>
    <ul id="ingred">
