3bb976b74b4526df447b0758f374fc2ca8eefa78
HoweBekah <how18019@byui.edu>
2019-11-11 16:36:23 -0700

    I hate npm

---
+++ web/nodeexpresspractice/ejspractice/testapp/app.js
@@ -0,0 +1,59 @@
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+
+var app = express();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
+// server.js
+// load the things we need
+
+// set the view engine to ejs
+// use res.render to load up an ejs view file
+
+// index page
+app.get("/", function(req, res) {
+  res.render("pages/index");
+});
+
+// about page
+app.get("/about", function(req, res) {
+  res.render("pages/about");
+});
+
+app.listen(8080);
+console.log("8080 is the magic port");
